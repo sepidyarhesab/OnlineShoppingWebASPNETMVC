6eb22efd7db15754daed2b41da62fb098db0e6e8
Neginjavaheri96 <fatemehjavaheri.ir@gmail.com>
2022-09-05 16:46:24 +0430

    Summary

--- OrdersInventory/Repository/Inventory/RepProductsSizeGuide.cs
+++ OrdersInventory/Repository/Inventory/RepProductsSizeGuide.cs
@@ -65,7 +65,7 @@ namespace OrdersInventory.Repository.Inventory
             try
             {
                 var search = searchnew.Replace("  ", "");
-                var searcha = string.IsNullOrWhiteSpace(searchnew);
+                //var searcha = string.IsNullOrWhiteSpace(searchnew);
                 var query = db.Table_Product_SizeGuide.OrderByDescending(c => c.CreatorDate).Where(c => c.Code == search || c.PrimaryTitle.Contains(search)).AsNoTracking().ToList();
                 if (query.Count > 0)
                 {
